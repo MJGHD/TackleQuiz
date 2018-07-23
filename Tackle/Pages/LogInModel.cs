f2186715c476a240f4ab8a956d4ce8dc10938414
MJGHD <myles.jon.gordon@gmail.com>
2018-07-23 16:12:38 +0100

    Initial commit

---
+++ Tackle/Pages/LogInModel.cs
@@ -0,0 +1,34 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tackle.Pages
+{
+    class LogInModel : PropertyChangedBase
+    {
+        private string _username;
+        private string _unencryptedPassword;
+        private string _buttonClickable;
+
+        public string Username
+        {
+            get { return this._username; }
+            set { SetAndNotify(ref this._username, value); }
+        }
+
+        public string UnencryptedPassword
+        {
+            get { return this._unencryptedPassword; }
+            set { SetAndNotify(ref this._unencryptedPassword, value); }
+        }
+
+        public string ButtonClickable
+        {
+            get { return this._buttonClickable; }
+            set { SetAndNotify(ref this._buttonClickable, value); }
+        }
+    }
+}
---
+++ Tackle/Pages/LogInViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Tackle.Pages
+{
+    class LogInViewModel
+    {
+        //Property containing all of the properties in the LogInModel
+        public LogInModel Details { get; set; }
+
+        public LogInViewModel()
+        {
+            Details = new LogInModel();
+            Details.ButtonClickable = "True";
+        }
+
+        public void LogInSubmit()
+        {
+            Details.ButtonClickable = "False";
+            string encryptedPassword = EncryptPassword();
+        }
+        public void SignUpSubmit()
+        {
+            Details.ButtonClickable = "False";
+            string encryptedPassword = EncryptPassword();
+        }
+
+        private string EncryptPassword()
+        {
+            /*
+            Converts the unencrypted password into a byte array, encrypts it using SHA256, and then converts it 
+            back into a string
+            */
+            byte[] byteArray = System.Text.Encoding.ASCII.GetBytes(Details.UnencryptedPassword);
+            byteArray = new System.Security.Cryptography.SHA256Managed().ComputeHash(byteArray);
+            string encryptedPassword = System.Text.Encoding.ASCII.GetString(byteArray);
+
+            return encryptedPassword;
+        }
+    }
+}
